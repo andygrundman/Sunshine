e652eba9e00121768c9f13ccc517cc3419f74d41
Andy Grundman <andy@hybridized.org>
2026-02-18 20:14:26 -0500

    Bump

--- cmake/compile_definitions/common.cmake
+++ cmake/compile_definitions/common.cmake
@@ -154,7 +154,7 @@ list(APPEND SUNSHINE_EXTERNAL_LIBRARIES
         enet
         libdisplaydevice::display_device
         nlohmann_json::nlohmann_json
-        opus
+        ${Opus_LIBRARY}
         ${FFMPEG_LIBRARIES}
         ${Boost_LIBRARIES}
         ${OPENSSL_LIBRARIES}
